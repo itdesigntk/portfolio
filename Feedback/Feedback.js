1ea132390f58ce74d6c1ef58ca2f38b65c80dfa1
itdesigntk <64609807+itsisaac19@users.noreply.github.com>
2020-07-03 09:43:47 -0500

    Create Feedback.js

---
+++ Feedback/Feedback.js
@@ -0,0 +1,29 @@
+//On body load hide elements
+
+function hide() {
+
+    bar.style.opacity = "0.001"
+}
+
+
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var bar = document.getElementById("bar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= 21) {
+    navbar.classList.add("sticky")
+    bar.classList.add("shadow")
+    bar.style.opacity = "1"
+  } else {
+    navbar.classList.remove("sticky");
+    bar.classList.remove("shadow");
+    bar.style.opacity = "0.001"
+  }
+  if (window.pageYOffset == 0) {
+    bar.style.opacity = "0.001"
+  }
+}
