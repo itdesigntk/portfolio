0121f33eb1450abce5a739643b48f8543c657e56
itdesigntk <64609807+itsisaac19@users.noreply.github.com>
2020-07-04 11:36:57 -0500

    Update Portfolio.js

--- Portfolio/Portfolio.js
+++ Portfolio/Portfolio.js
@@ -5,29 +5,10 @@ function hide() {
 
     bar.style.opacity = "0.001"
 
-    sortorder ();
-}
-
-function sortorder () {
-
-  var photos = 5
-
-  var lasttile = "tile" + photos
-  var thirdtile = "tile" + (photos - (photos - 3)) 
-  var secondtile = "tile" + (photos - (photos - 2)) 
-  var firsttile = "tile" + (photos - (photos - 1)) 
-
-
-  var two = "'https://i.ibb.co/23zPmfP/IMG-0781.jpg'"
-  var one = "'https://i.ibb.co/23zPmfP/IMG-0781.jpg'"
-  var zero = "'https://i.ibb.co/rp9w6Gf/70997517-1181052828748245-4085072603278330813-n.jpg'"
     
-  document.getElementById(firsttile).style.backgroundImage = "url("+first+")";
-
-  document.getElementById(lasttile).style.backgroundImage = "url("+zero+")";
-
-  // I have no idea what im doing
 }
+
+
 
 window.onscroll = function() {myFunction()};
 
