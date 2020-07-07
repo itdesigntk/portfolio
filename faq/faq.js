0043f1d7cc7d8ec409905957c0d2504aee698086
itdesigntk <64609807+itsisaac19@users.noreply.github.com>
2020-07-07 09:00:53 -0500

    Create faq.js

---
+++ faq/faq.js
@@ -0,0 +1,39 @@
+//On body load hide elements
+
+function hide() {
+
+    bar.style.opacity = "0.001"
+}
+
+
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var bar = document.getElementById("bar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= 21) {
+    navbar.classList.add("sticky")
+    bar.classList.add("shadow")
+    bar.style.opacity = "1"
+  } else {
+    navbar.classList.remove("sticky");
+    bar.classList.remove("shadow");
+    bar.style.opacity = "0.001"
+  }
+  if (window.pageYOffset == 0) {
+    bar.style.opacity = "0.001"
+  }
+}
+
+function chooseon() {
+  document.getElementById("overlaychoose").style.display = "block"
+  document.getElementById("choosetemplate").style.display = "block"
+}
+
+function chooseoff() {
+  document.getElementById("overlaychoose").style.display = "none"
+  document.getElementById("choosetemplate").style.display = "none"
+}
