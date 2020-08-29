502cf16b1d8022d50e2183a56f6ec7e4398099ac
itdesigntk <66639356+itdesigntk@users.noreply.github.com>
2020-08-28 22:31:22 -0500

    BathroomStand
    Added Card for Tile 1 on Projects: 'Bathrrom Stand'

--- Projects/Projects.js
+++ Projects/Projects.js
@@ -24,5 +24,51 @@ function myFunction() {
     bar.classList.remove("shadow");
     bar.style.opacity = "0.001"
   }
-  fadein();
+}
+
+function expandTile1() {
+
+  var visibility = document.getElementById("mainCard").style.display    
+
+  //console.log(visibility)
+
+  if (visibility === "") {
+    visibility = "none"
+  }
+
+  //console.log(visibility)
+  
+  switch (visibility) {
+
+    default: 
+      alert("default case triggered")
+
+    case "none":
+
+      document.getElementById("mainCard").style.display = "block"
+      document.getElementById("tile2").style.display = "none"
+      document.getElementById("tile3").style.display = "none"
+      document.getElementById("tile4").style.display = "none"
+      document.getElementById("tile5").style.display = "none"
+      document.getElementById("tile6").style.display = "none"
+      document.getElementById("tile7").style.display = "none"
+      document.getElementById("tile8").style.display = "none"
+      document.getElementById("tile9").style.display = "none"    
+      break;
+
+    case "block":
+
+      document.getElementById("mainCard").style.display = "none"
+      document.getElementById("tile2").style.display = "block"
+      document.getElementById("tile3").style.display = "block"
+      document.getElementById("tile4").style.display = "block"
+      document.getElementById("tile5").style.display = "block"
+      document.getElementById("tile6").style.display = "block"
+      document.getElementById("tile7").style.display = "block"
+      document.getElementById("tile8").style.display = "block"
+      document.getElementById("tile9").style.display = "block"
+      break;
+
+  }
+
 }
